608b6ce0601c4b02062d3af5b9003404b34a0da2
TakamiRena <rena.takami@g.dreamcareer.co.jp>
2025-06-09 17:38:57 +0900

    見た目完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,74 @@
-import "./App.css";
+import {
+  AppBar,
+  Box,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import type { Todo } from "./types";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Ubuntu", '"Noto Sans JP"', "sans-serif"].join(","),
+  },
+  palette: {
+    background: {
+      default: "#e0e0e0",
+      paper: "#fff",
+    },
+  },
+});
+
 function App() {
-  const dummyTodos: Todo[] = [
-    { id: "1", task: "買い物", isCompleted: false },
-    { id: "2", task: "掃除", isCompleted: true },
+  const dummyUncompleteTodos: Todo[] = [
+    {
+      id: "1",
+      task: "買い物(tomato)",
+      isCompleted: false,
+    },
+    { id: "2", task: "掃除", isCompleted: false },
     { id: "3", task: "洗濯", isCompleted: false },
   ];
 
   return (
-    <>
-      <h1>Todoアプリ</h1>
-      <TodoForm />
-      <TodoList todos={dummyTodos} />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h5" component="h1">
+            todo-list
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* メインコンテナ */}
+      <Box sx={{ maxWidth: "md", m: "0 auto", p: 3, mt: 7 }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            mb: 2,
+          }}
+        >
+          <TodoForm />
+        </Box>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            boxShadow: 2,
+            borderRadius: 2,
+            p: 3,
+            mt: 2,
+          }}
+        >
+          <TodoList todos={dummyUncompleteTodos} />
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,22 @@
 import { Box, Button, TextField } from "@mui/material";
 
 export default function TodoForm() {
-    return (
-        <>
-            <Box>
-                <TextField label="タスクを入力" variant="standard" />
-                <Button variant="contained">追加</Button>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 1,
+          bgcolor: "background.paper",
+          p: 2,
+          borderRadius: 2,
+          boxShadow: 2,
+        }}
+      >
+        <TextField label="タスクを入力" variant="outlined" />
+        <Button variant="outlined">追加</Button>
+      </Box>
+    </>
+  );
 }
